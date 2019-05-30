a226e92f8d57119c85d5b94ff7f7faa7abcb3256
Ashwin <ashwin.dinesh01@gmail.com>
2019-05-30 16:19:49 +0530

    first commit

---
+++ app/src/main/java/com/ashwin/android/example/volleykotlin/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.ashwin.android.example.volleykotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+    lateinit var queue: RequestQueue
+    val url = "https://gist.githubusercontent.com/ashwindmk/1e2097ac3de60a40c469ec1a60f35b41/raw/profile.json"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        queue = Volley.newRequestQueue(this)
+
+        swipetorefresh_layout.setOnRefreshListener {
+            request()
+        }
+    }
+
+    private fun request() {
+        response_textview.text = "Fetching..."
+
+        val jsonRequest = JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            Response.Listener<JSONObject> { response ->
+                response_textview.text = "${response.toString(2)}"
+                swipetorefresh_layout.isRefreshing = false
+            },
+            Response.ErrorListener {
+                response_textview.text = "That didn't work!"
+                swipetorefresh_layout.isRefreshing = false
+            }
+        )
+
+        jsonRequest.setShouldCache(false)
+
+        queue.add(jsonRequest)
+    }
+}
